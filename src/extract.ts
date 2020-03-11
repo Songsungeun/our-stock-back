56f41c8711765d60fcb96fac0bd4131e61e5790d
Songsungeun <korea.sesong@gmail.com>
2020-03-11 13:48:46 +0900

    나스닥 파싱 추가

--- src/extract.ts
+++ src/extract.ts
@@ -3,17 +3,29 @@ import { load } from 'cheerio'
 export const extract = (html: string, type?: string) => {
 	if (html === '') return [];
 	const $ = load(html);
-	let val, rate, data = { val: '', rate: '' };
+	let data = { val: '', rate: '' };
 
 	if (type && type === 'nasdaq') {
-		val = $('#dayTable > .tb_td2:first-child');
-		console.log(val.text());
-	}
+		data.val = $('#dayTable > tbody .tb_td2').first().text();
+		let rateVal = $('#dayTable > tbody .tb_td3').first().text();
+		let className = $('#dayTable > tbody > tr').first();
 
-	val = $('#now_value'), rate = $('#change_value_and_rate');
-	data.val = val.text();
-	data.rate = rate.text().split('%')[0];
+		// ClassName으로 up인지 down인지 판별
+		let upDown = className.attr('class')?.replace(/(\s*)/g, "") === 'point_up' ? '+' : '-';
+		// nasdaq은 등락비율 제공을 안해줘서 별도로 등락비율 계산
+		data.rate = `${rateVal} ${upDown}${calcRate(data.val, rateVal, upDown).toFixed(2)}`;
+
+	} else {
+		data.val = $('#now_value').text();
+		data.rate = $('#change_value_and_rate').text().split('%')[0];
+	}
 
 	return data;
 
+}
+
+const calcRate = (todayIndex: string, differenceVal: string, updown: string) => {
+	todayIndex = todayIndex.replace(",", ""), differenceVal = differenceVal.replace(",", "");
+	let yesterdayIndex = updown === 'up' ? (Number(todayIndex) - Number(differenceVal)) : (Number(todayIndex) + Number(differenceVal));
+	return (Number(differenceVal) / yesterdayIndex) * 100;
 }
